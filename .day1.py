7b322272da5047076c1bb88136d9b59248b5e250
siddhimane900 <siddhimane0888@gmail.com>
2026-04-05 22:26:50 +0530

    Add files via upload

---
+++ .day1.py
@@ -0,0 +1,12 @@
+print("Hello Guy's..")
+name=input("Enter the Name ")
+age=int(input("Enter the Age "))
+if age >=18:
+    print("Eligible for voting")
+else:
+    print("Not Eligible for Voting")
+    print("Your Age Should be above 18")
+for i in range(4):
+    print("Number :",i)
+for i in range(1,8):
+     print("Number :",i)
